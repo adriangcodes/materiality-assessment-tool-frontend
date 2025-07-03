fb05fbac0dcef6b51541409b2344bed2ccc45546
Adrian Gidaro <adriangidaro@gmail.com>
2025-07-03 23:55:43 +1000

    Home page first draft

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './pages/LandingPage.jsx'
+import HomePage from './pages/HomePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HomePage />
   </StrictMode>,
 )
