28b827ac9a020134048f39fa970a008bec1465db
Adrian Gidaro <adriangidaro@gmail.com>
2025-07-04 21:56:59 +1000

    Updated routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import Home from './pages/Home.jsx'
 import Survey from './pages/Survey.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <HomePage />
-//   </StrictMode>,
-// )
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/survey/:id" element={<SurveyPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/survey/:id" element={<Survey />} />
       </Routes>
     </BrowserRouter>
   )
 }
 
 export default App
+
+createRoot(document.getElementById('root')).render(
+  <App />
+)
