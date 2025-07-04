146b6ad61c3dc80718f7fa1a8ada148551136450
Adrian Gidaro <adriangidaro@gmail.com>
2025-07-04 22:12:57 +1000

    Survey page formatting - first draft

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,20 +1,28 @@
 import { useParams } from 'react-router-dom'
+import Logo from '../components/Logo'
+import HomePageFooter from '../components/HomePageFooter'
 
 function Survey() {
   const { id } = useParams()
 
-  // Placeholder for fetching survey data in the future
-  // useEffect(() => {
-  //   fetch(`https://materiality-assessment-tool.onrender.com/survey/${id}`)
-  //     .then(res => res.json())
-  //     .then(data => { ... });
-  // }, [id]);
-
   return (
-    <div>
-      <h1>Survey Form</h1>
-      <p>Survey ID: {id}</p>
-      {/* Survey form will go here */}
+    <div className="landing-root">
+      <header className="landing-header">
+        <div className="logo-container">
+          <a href="/" aria-label="Go to home page">
+            <Logo />
+          </a>
+          <span className="header-title">Materiality Assessment Tool</span>
+        </div>
+      </header>
+      <main className="landing-main">
+        <div className="survey-content">
+          <h1>Survey Form</h1>
+          <p>Survey ID: {id}</p>
+          {/* Survey form will go here */}
+        </div>
+      </main>
+      <HomePageFooter />
     </div>
   )
 }
